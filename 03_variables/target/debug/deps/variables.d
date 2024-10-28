6b5d7d78859c611d29a511f4d0cac54111236614
PJLys <pjlyssens@gmail.com>
2024-10-28 12:02:21 +0400

    Initial variables code

---
+++ 03_variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+C:\Users\pjlys\Documents\Pieter-Jan\Thuisprojecten\Rust Walkthrough\03_variables\target\debug\deps\variables.exe: src/main.rs
+
+C:\Users\pjlys\Documents\Pieter-Jan\Thuisprojecten\Rust Walkthrough\03_variables\target\debug\deps\variables.d: src/main.rs
+
+src/main.rs:
