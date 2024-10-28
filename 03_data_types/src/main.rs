a0f2ec5b86be4f64ee4fe6fd50b32965b49b6cfd
PJLys <pjlyssens@gmail.com>
2024-10-28 12:55:08 +0400

    Data Types

---
+++ 03_data_types/src/main.rs
@@ -0,0 +1,42 @@
+use std::io;
+/*
+    Data Types
+    - Scalar
+        - Unsigned Int: u8-128
+        - Signed Int: i8-128 (Two's complement)
+        - arch: isize/usize
+    - Number literals
+        - Separator '_'
+        - Hex 0x...
+        - Octal 0o...
+        - Binary 0b...
+        - byte (u8) b'...'
+    - Real
+        - f32, f64 (default)
+    - chars
+        - 4 bytes
+        - single quotes
+    - Compounds
+        - Tuple 
+        - Array
+*/
+fn main() {
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
+}
--- 03_variables/src/main.rs
+++ 03_variables/src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
-    let x = 5;
+    let mut x = 5;
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
 }
+
+/*
+    Shadowing
+    = Reuse the same variable name, while changing the value and/or type of the data.
+    Most likely use case: Reading from IOStream and converting the data into usable format.
+*/
