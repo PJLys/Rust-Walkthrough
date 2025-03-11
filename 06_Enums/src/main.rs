1453f15c1efa1e0c6aa0f7acd6c2f00544aea70f
PJLys <pjlyssens@gmail.com>
2025-03-11 12:04:45 +0100

    6.3 if-let

--- 06_Enums/src/main.rs
+++ 06_Enums/src/main.rs
@@ -46,7 +46,12 @@ fn main() {
 
     let x: i8 = 5;
 
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {max}")
+    }
 }
+
 
 
 
