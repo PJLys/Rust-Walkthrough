d701674db88ff8d268971f78d64069a948f6a310
PJLys <pjlyssens@gmail.com>
2025-03-11 11:25:39 +0100

    6.2 match

--- 06_Enums/src/main.rs
+++ 06_Enums/src/main.rs
@@ -4,14 +4,26 @@ enum IpAddr {
     V6(String),
 }
 
-enum Message {
-    Quit,
-    Move {x: i32, y: i32},
-    Write(String),
-    ChangeColor(i32, i32, i32)
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter
+}
+
+fn value_in_cents(coin:Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25
+    }
 }
 
 
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
+fn reroll() {}
 
 fn main() {
     let home = IpAddr::V4(127, 0, 0, 1);
@@ -19,6 +31,13 @@ fn main() {
 
     println!("{:#?}", loopback);
     println!("{:#?}", home);
+    
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        _ => reroll(),
+    }
 
     let some_number = Some(5);
     let some_char = Some('5');
@@ -27,7 +46,6 @@ fn main() {
 
     let x: i8 = 5;
 
-    let sum = x + some_number;
 }
 
 
